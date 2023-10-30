fe0a6995fcaf98c943b5fe4c9ac1e847a27cdc5b
Garry <garry@azurflow.com>
2023-10-30 21:26:10 +0800

    Update example main (#1)

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import cv2
+import logging
 from gridmap import GridmapLoader, LayeredGridmap, GridmapSaver
+from rich.logging import RichHandler
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s", datefmt="[%X]", handlers=[RichHandler(highlighter=None)])
 
 if __name__ == '__main__':
+    logger = logging.getLogger("rich")
+    logger.info("Program start!")
     
-    # As default, the color of item '0' is background color (white)
-    layer_1 = GridmapLoader.Load1BitBMP('./input/Layer1_Obstacle_DemoCross_1000x1000_1Bit.bmp').set_name('layer_1').replace(1, 1)
-    layer_2 = GridmapLoader.Load1BitBMP('./input/Layer2_AmberLines_DemoCross_1000x1000_1Bit.bmp').set_name('layer_2').replace(1, 2)
-    layer_3 = GridmapLoader.Load1BitBMP('./input/Layer3_StopLines_DemoCross_1000x1000_1Bit.bmp').set_name('layer_3').replace(1, 3)
-    layer_4 = GridmapLoader.Load1BitBMP('./input/Layer4_WhiteLines_DemoCross_1000x1000_1Bit.bmp').set_name('layer_4').replace(1, 4)
-    layer_5 = GridmapLoader.Load1BitBMP('./input/Layer5_CenterLines_DemoCross_1000x1000_1Bit.bmp').set_name('layer_5').replace(1, 5)
-        
+    # load gridmaps
+    logger.info('Loading 1-bit BMP file as Gridmap')
+    layer_1 = GridmapLoader.Load1BitBMP('./input/Layer1_Obstacle_DemoCross_1000x1000_1Bit.bmp', 'Layer 1 Obstacle')
+    layer_2 = GridmapLoader.Load1BitBMP('./input/Layer2_AmberLines_DemoCross_1000x1000_1Bit.bmp', 'Layer 2 AmberLines')
+    layer_3 = GridmapLoader.Load1BitBMP('./input/Layer3_StopLines_DemoCross_1000x1000_1Bit.bmp', 'Layer 3 StopLines')
+    layer_4 = GridmapLoader.Load1BitBMP('./input/Layer4_WhiteLines_DemoCross_1000x1000_1Bit.bmp', 'Layer 4 WhiteLines')
+    layer_5 = GridmapLoader.Load1BitBMP('./input/Layer5_CenterLines_DemoCross_1000x1000_1Bit.bmp', 'Layer 5 CenterLines')
+    
+    # reformate gridmap and save to list
+    layers = []
+    layers.append(layer_1.replace(1, 1))
+    layers.append(layer_2.replace(1, 2))
+    layers.append(layer_3.replace(1, 3))
+    layers.append(layer_4.replace(1, 4))
+    layers.append(layer_5.replace(1, 5))
+    
+    # generate layered gridmap
+    logger.info('Creating LayeredGridmap and apply overlay mode')
     lgm = LayeredGridmap(layer_1.width, layer_1.height)
-    lgm.add_layers([layer_1, layer_2, layer_3, layer_4, layer_5])    
+    lgm.add_layers(layers)
+    
+    # show images
+    logger.info(f'Show layers and overlayed images')
+    for layer in layers:
+        layer.visualizer.show_image()
     lgm.visualizer.show_image()
-    
-    GridmapSaver.SaveAsNumpy(lgm, './output/layered_gridmap.npy')
-    GridmapSaver.SaveAsBmp(lgm, './output/layered_gridmap.bmp')
-    
+    logger.warning(f'Please press any key or close all windows to continue ...')
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
+    logger.info('Saving LayeredGridmap as numpy and bmp file')
+    GridmapSaver.SaveAsNumpy(lgm, './output/layered_gridmap.npy')
+    GridmapSaver.SaveAsBmp(lgm, './output/layered_gridmap.bmp')
+    
+    logger.info('Program end!')
