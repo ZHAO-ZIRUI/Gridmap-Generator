2c4b69bdf45679fce16ee0de444179e59e473be9
Garry <garry@azurflow.com>
2023-10-30 20:37:24 +0800

    Allow named gridmap in loader (#1)

--- gridmap/gridmap_loader.py
+++ gridmap/gridmap_loader.py
@@ -5,13 +5,13 @@ from .gridmap import Gridmap
 class GridmapLoader:
     
     @classmethod
-    def Load1BitBMP(cls, path: str):
+    def Load1BitBMP(cls, path: str, name = '') -> Gridmap:
         if not os.path.exists(path):
             raise FileNotFoundError(path)
         if not path.lower().endswith('.bmp'):
             raise ValueError('Only support BMP file')
         img = Image.open(path)
-        gridmap = Gridmap(img.width, img.height)
+        gridmap = Gridmap(img.width, img.height, name)
         for y in range(img.height):
             for x in range(img.width):
                 gridmap.data[x, y] = img.getpixel((x, y))
